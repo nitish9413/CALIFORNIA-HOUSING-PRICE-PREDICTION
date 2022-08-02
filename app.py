e7000bdc224ea7a5962736a2f3988a13d2e1ffb7
nitish9413 <nitishkatkade94@gmail.com>
2022-08-02 23:12:58 +0530

    t2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 
 def index():
-    return 'starting...'
+    return 'CI/CD pipeline is deployed'
 
 if __name__ == '__main__':
     app.run(debug=True)

6ea873617aa12042a9c05ad2f842333470c2c3b3
nitish9413 <nitishkatkade94@gmail.com>
2022-08-08 09:55:54 +0530

    data validation

--- demo.py
+++ demo.py
@@ -1,10 +1,12 @@
-from housing.pipeline.pipeline import Pipeline
 from housing.exception import HousingException
 from housing.logger import logging
+from housing.config.configuration import Configuration
 def main():
     try:
-        pipeline =Pipeline()
-        pipeline.run_pipeline()
+        # pipeline =Pipeline()
+        # pipeline.run_pipeline()
+        data_validation_config = Configuration().get_data_validation_config()
+        print(data_validation_config)
     except Exception as e:
         logging.error(f"{e}")
         print(e)
--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -25,3 +25,12 @@ DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY = "tgz_download_dir"
 DATA_INGESTION_INGESTED_DIR_NAME_KEY = "ingested_dir"
 DATA_INGESTION_TRAIN_DIR_KEY = "ingested_train_dir"
 DATA_INGESTION_TEST_DIR_KEY = "ingested_test_dir"
+
+
+# Data Validation related variable
+DATA_VALIDATION_CONFIG_KEY = "data_validation_config"
+DATA_VALIDATION_SCHEMA_FILE_NAME_KEY = "schema_file_name"
+DATA_VALIDATION_SCHEMA_DIR_KEY = "schema_dir"
+DATA_VALIDATION_ARTIFACT_DIR = "data_validation"
+DATA_VALIDATION_REPORT_FILE_NAME_KEY = "report_file_name"
+DATA_VALIDATION_REPORT_PAGE_FILE_NAME_KEY = "report_page_file_name"

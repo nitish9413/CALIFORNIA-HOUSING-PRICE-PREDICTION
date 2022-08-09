d79c772f6f7b760f7a4055fb1815c8887d3f4af3
nitish9413 <nitishkatkade94@gmail.com>
2022-08-09 14:01:04 +0530

    data transformation added

--- housing/components/data_transformation.py
+++ housing/components/data_transformation.py
@@ -8,7 +8,7 @@ import os,sys
 
 class DataTransformation:
 
-    def __init__(self,data_trnasformation_config: DataTransformationConfig,
+    def __init__(self,data_transformation_config: DataTransformationConfig,
                 data_ingestion_artifact: DataIngestionArtifact,
                 data_validation_artifact: DataValidationArtifact   
                 ):
